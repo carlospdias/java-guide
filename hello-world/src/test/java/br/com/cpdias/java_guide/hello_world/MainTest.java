7ff96291ae4ad47e29283b234027102edfc767e3
Carlos Pereira Dias <carlospdiase@gmail.com>
2025-05-29 21:43:06 -0300

    :tada: Cria Exemplo de 'Hello Word' Java

---
+++ hello-world/src/test/java/br/com/cpdias/java_guide/hello_world/MainTest.java
@@ -0,0 +1,14 @@
+package br.com.cpdias.java_guide.hello_world;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class MainTest {
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
